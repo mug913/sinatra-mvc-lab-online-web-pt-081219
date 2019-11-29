5a5c16519b5976e28c3ea570294017e5b4dc61cf
Brad Alexander <brad.alexander913@gmail.com>
2019-11-29 17:08:10 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,7 +10,7 @@ class PigLatinizer
     if letters[-1] =~ /[aeiouAEIOU]/
       piglatinized = letters.join("") + "way"
     else
-      if letters[-1] =~ /[hHsSpPlLtT]/
+      if letters[-1] =~ /[hHsSpPlLtT]/ && letters[-2] !=~ /[aeiouAEIOU]/
         piglatinized = letters.join("") + "ay"
       else
         piglatinized = letters.join("") + "way"
