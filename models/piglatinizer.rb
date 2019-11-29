27f1d3f991735c8c25f9ca46914ec1db3a6a0460
Brad Alexander <brad.alexander913@gmail.com>
2019-11-29 16:48:03 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,7 +7,7 @@ class PigLatinizer
 
   def piglatinize(words)
     letters = self.split(words)
-    binding.pry
+  #  binding.pry
     if letters[-1] =~ /[aeiouAEIOUlL]/
       piglatinized = letters.join("") + "way"
     else
